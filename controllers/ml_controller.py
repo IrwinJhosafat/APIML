26ca69af03d08213b514a896f8b65fd21f534bfb
IrwinJhosafat <ijflores@frioexpress.com>
2023-09-10 09:16:43 -0600

    Version Final
    Version Final

--- controllers/ml_controller.py
+++ controllers/ml_controller.py
@@ -5,19 +5,13 @@ import numpy as np
 
 from tensorflow import keras
 
-#MODEL_PATH= 'pulsar_model.h5'
+MODEL_PATH= 'pulsar_model.h5'
 #Load TensorFlow Model
-#model = keras.models.load_model(MODEL_PATH)
+model = keras.models.load_model(MODEL_PATH)
 
 router = APIRouter()
 
-preds = [
-
-{"id": 1, "pred":0.56264937},
-{"id": 2, "pred":0.0496036 },
-{"id": 3, "pred":0.00634932},
-{"id": 4, "pred":0.02918332}
-]
+preds = []
 
 @router.get("/ml")
 def get_preds():
@@ -26,8 +20,7 @@ def get_preds():
 @router.post("/ml",status_code=status.HTTP_201_CREATED, response_model=Prediction_Output)
 def predict(pred_input: Prediction_Input):
     input_data = np.array([pred_input.Mean_DMSNR_Curve,pred_input.SD_DMSNR_Curve,pred_input.EK_DMSNR_Curve,pred_input.Skewness_DMSNR_Curve])
- #   prediction =  model.predict(input_data)
-    prediction = 0.56264937  
+    prediction =  model.predict(input_data) 
     result = {"id": str(pred_input.id), "pred":prediction}
     preds.append(result)
 
